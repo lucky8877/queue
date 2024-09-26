6b519bdcb65395e659eaa772060efbb9c34a5f27
Vlaislav <itserwhite@gmail.com>
2024-09-26 23:02:38 +0300

    update Doctor.h

--- PatientClass/Doctor.h
+++ PatientClass/Doctor.h
@@ -1,11 +1,13 @@
 #pragma once
-#include <patient.h>
+#include "patient.h"
+
 
 class Doctor {
-    string*_drugs {};
+    string _drugs[3] {"no-spa", "nurofen", "kagocel"};
+    Patient *_currentPatient;
     public:
-        void TakePatient(Patient* p);
-        void PrintProblem(Patient* p);
+        void TakePatient(Patient *p);
+        void PrintProblem(Patient *p);
         void FillDiagnosis(Diag *diag);
 };
 
